e1214a37492f628bd22bea5a521e4f4c4d60cc61
maudmain <117314424+maudmain@users.noreply.github.com>
2023-02-03 23:17:18 +0000

    display current weather fucntion added

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,8 @@ const API = "5e1d2997c137aaf57841dd7d335e490e";
 // event listener
 $("#search-button").on("click", function(event){
     event.preventDefault();
+
+    clear();
     
     const cityQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=bristol&limit=1&appid=5e1d2997c137aaf57841dd7d335e490e";
 
@@ -24,24 +26,47 @@ function weatherQuery(response){
 
     let lat = response[0].lat
     let lon = response[0].lon
-    const weatherQueryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}`
-   
+    const weatherQueryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API}`
 
+    const fiveDayQueryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&cnt=5&appid=${API}`
+   
     $.ajax({
         url: weatherQueryURL,
         method: "GET"
-      }).then(weatherDisplay);   
+      }).then(weatherDisplay);
 
-
+      $.ajax({
+        url: fiveDayQueryURL,
+        method: "GET"
+      }).then(fiveDayDisplay);
 }
 
 function weatherDisplay(response){
-    console.log(response)
+    console.log(response);
 
-    let currentWeather = $("<div>")
+    let currentWeather = $("<div class='current-weather>");
+    $("#weather-container").append(currentWeather );
 
     let currentdate = moment().format("L");
 
-    let cityName = $("<h2>").text(response.name)
-    $("#weather-container").append(cityName  )
+    let cityName = $("<h2>").text(response.name + ' (' + currentdate + ')');
+    let iconCurrent = response.weather[0].icon;
+    console.log(iconCurrent)
+    let currentWeatherIcon = $("<img>").attr('src', "https://openweathermap.org/img/w/" + iconCurrent + ".png")
+
+    let currentTemp = $("<p>").text('Temp: ' + response.main.temp + 'C')
+    let currentWind = $("<p>").text('Wind:' + response.wind.speed + ' meter/sec');
+    let currentHumidity =$("<p>").text('Humidity: ' + response.main.humidity + '%') ;
+
+    $("#weather-container").append(cityName,currentWeatherIcon, currentTemp, currentWind,currentHumidity );
 }
+
+function fiveDayDisplay(response){
+    console.log(response)
+
+}
+
+// Function to empty out the articles
+function clear() {
+    $("#weather-container").empty();
+  }
